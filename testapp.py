dff87541771335c124e4da550f9e23cfb0c0cbe6
nikulson <nikulshin@mail.ru>
2022-05-21 16:10:03 +0300

    Add file

---
+++ testapp.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+
+@app.route('/about')
+def about():
+    return "about page"
+
+if __name__ == "__main__":
+    app.run(debug=True)
