28877a8e325e2a56436500ccb304d5cf0ee1592a
nikulson <nikulshin@mail.ru>
2022-05-31 23:00:01 +0300

    Add file

--- testapp.py
+++ testapp.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -8,9 +8,21 @@ def index():
     return "Hello, World!"
 
 
-@app.route('/about')
-def about():
-    return "Downloading..."
+@app.route('/dogs', methods=["POST", "GET",])
+def dogs():
+    return "dogs"
+
+
+@app.route('/dogs/<id>', methods=["GET", "POST", "DELETE"])
+def createdogs(id):
+    request.method
+    if request.method == "GET":
+        return f"dog:{id}. method was: GET"
+    if request.method == "POST":
+        return f"dog:{id}. method was: POST"
+    if request.method == "DELETE":
+        return f"dog:{id}. method was: DELETE"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
